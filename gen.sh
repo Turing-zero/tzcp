9288588e7d38c5fa01d20c42c6b3644799167ac0
mark <mark_zjunlict@zju.edu.cn>
2024-03-23 15:54:50 +0800

    update gen.sh

--- gen.sh
+++ gen.sh
@@ -1,24 +1,30 @@
 #!/bin/bash
 
-OUTPUT_DIR="tzcp"
+# $1 input files dir , $2 output dir
+if [ -z $1 ]; then
+    echo "Usage: gen.sh input_dir [output_dir=tzcp]"
+    exit 1
+fi
+
+if [ -z $2 ]; then
+    OUTPUT_DIR="tzcp"
+else
+    OUTPUT_DIR=$2
+fi
+
 mkdir -p $OUTPUT_DIR
 
-# get all args to list
-args=("$@")
-
+dir_name=$1
 all_files=()
 include_dirs=()
-for dir_name in "${args[@]}"
-do
-    include_dirs+=("-I$dir_name")
-    # get all .proto file in dir
-    proto_files=$(find $dir_name -name "*.proto")
-    for file in $proto_files
-    do
-        all_files+=($file)
-    done
-done
 
+include_dirs+=("-I$dir_name")
+# get all .proto file in dir
+proto_files=$(find $dir_name -name "*.proto")
+for file in $proto_files
+do
+    all_files+=($file)
+done
 
 # generate files
 protoc ${include_dirs[@]} ${all_files[@]} --cpp_out=$OUTPUT_DIR --python_out=$OUTPUT_DIR
