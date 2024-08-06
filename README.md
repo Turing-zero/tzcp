9a18b59f6c8a2b01f8247cc9ecc89af2415f673c
mark <mark_zjunlict@zju.edu.cn>
2024-08-06 20:47:38 +0800

    update gen.sh

--- README.md
+++ README.md
@@ -2,3 +2,13 @@
 > Turing-zero communication protocol
 
 Used by [tbk](https://github.com/Turing-zero/TBK) & [tbox](https://github.com/Turing-zero/tbox) & ...
+
+
+
+> how to use
+
+```bash
+# change directory to your custom workspace 
+/xxx/xxx/tzcp/gen.sh [output_dir=tzcp_gen] # output_dir can leave as empty [default is 'tzcp_gen']
+```
+
--- gen.sh
+++ gen.sh
@@ -1,19 +1,13 @@
 #!/bin/bash
 
-# echo "Usage: gen.sh [input_dir=/absolute/to/tzcp] [output_dir=tzcp]"
-
-# $1 input files dir , $2 output dir
+# echo "Usage: gen.sh [output_dir=tzcp_gen]"
+SCRIPT_DIR="$( dirname -- "${BASH_SOURCE[0]}" )"
+INPUT_DIR=$SCRIPT_DIR
+# $1 output dir
 if [ -z $1 ]; then
-    SCRIPT_DIR="$( dirname -- "${BASH_SOURCE[0]}" )"
-    INPUT_DIR=$SCRIPT_DIR
-else
-    INPUT_DIR=$1
-fi
-
-if [ -z $2 ]; then
     OUTPUT_DIR="tzcp_gen"
 else
-    OUTPUT_DIR=$2
+    OUTPUT_DIR=$1
 fi
 
 echo "input dir  : $INPUT_DIR"
